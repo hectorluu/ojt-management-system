252160eb0c51a2ce5c891f6572ba1898f5b4797b
Hung Luu <luuduchunghmd1619@gmail.com>
2023-09-24 14:32:34 +0700

    work history trainee page

--- src/views/pages/Trainer/TraineeDetailPage.js
+++ src/views/pages/Trainer/TraineeDetailPage.js
@@ -272,27 +272,40 @@ const TraineeDetailPage = () => {
           Nhật ký công việc
         </Typography>
         <div className="relative px-4">
-          <div className="absolute h-full border border-dashed border-opacity-60 border-secondary"></div>
+          {traineeTask.length !== 0 && (
+            <div className="absolute h-full border border-dashed border-opacity-60 border-secondary"></div>
+          )}
 
           {/* Timeline item */}
-          {traineeTask.map((task) => (
-            <div
-              className="flex items-center w-full my-6 -ml-1.5"
-              key={task.id}
-            >
-              <div className="w-1/12 z-10">
-                <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
+          {traineeTask.length > 0 &&
+            traineeTask.map((task) => (
+              <div
+                className="flex items-center w-full my-6 -ml-1.5"
+                key={task.id}
+              >
+                <div className="w-1/12 z-10">
+                  <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
+                </div>
+                <div className="w-11/12">
+                  <Typography variant="body1" className="text-sm">
+                    {task.name}
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    className="text-xs text-gray-500"
+                  >
+                    Thời gian hoàn thành: {fDateTime(task.finishTime)}
+                  </Typography>
+                </div>
               </div>
-              <div className="w-11/12">
-                <Typography variant="body1" className="text-sm">
-                  {task.name}
-                </Typography>
-                <Typography variant="caption" className="text-xs text-gray-500">
-                  Thời gian hoàn thành: {fDateTime(task.finishTime)}
-                </Typography>
-              </div>
+            ))}
+          {traineeTask.length === 0 && (
+            <div className="flex items-center w-full my-6">
+              <Typography variant="body1" className="text-base font-medium">
+                Thực tập sinh này chưa có nhật ký công việc.
+              </Typography>
             </div>
-          ))}
+          )}
           {/* End Timeline item */}
         </div>
       </Paper>
