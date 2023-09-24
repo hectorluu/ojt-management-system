9d6404aa13be97e5cd40ac2e36a633ca4a8028c2
Hung Luu <luuduchunghmd1619@gmail.com>
2023-09-24 11:39:27 +0700

    logo

--- src/views/layout/LayoutAuthentication.js
+++ src/views/layout/LayoutAuthentication.js
@@ -33,14 +33,18 @@ const LayoutAuthentication = (props) => {
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
   return (
-    <div className="relative w-full min-h-screen p-10 bg-slate-300 dark:bg-darkbg isolate">
+    <div className="relative w-full min-h-screen p-10 bg-slate-300 dark:bg-darkbg isolate flex flex-col justify-center items-center">
       <img
         src="/ellipse.png"
         alt="bg"
         className="hidden lg:block absolute bottom-0 left-0 right-0 pointer-events-none z-[-1]"
       />
-      <Link to="/login" className="inline-block mb-5 lg:mb-16">
-        <img srcSet="/logo.png 2x" alt="ojt-management-system" />
+      <Link to="/login" className="inline-block">
+        <img
+          srcSet="/deralogo.png 3x"
+          alt="ojt-management-system"
+          className="object-fill"
+        />
       </Link>
       <div className="w-full max-w-[556px] bg-white dark:bg-darkSecondary rounded-xl px-5 py-8 lg:px-16 lg:py-12 mx-auto">
         <h1 className="mb-1 text-lg font-semibold text-center lg:text-xl lg:mb-3 text-text1 dark:text-white">
